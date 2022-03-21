9376aeb9c4b1bb46299dc25909282bb392a0684b
soyakimm <100359222+soyakimm@users.noreply.github.com>
2022-03-21 21:39:29 +0900

    [Bronze III] Title: A+B - 3, Time: 232 ms, Memory: 17960 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		int T=scan.nextInt(); // T가 주어진다.
+
+		while(T-- >0) 
+		{
+			int A=scan.nextInt();
+			int B=scan.nextInt();
+			
+			System.out.println(A+B);
+		}
+	}
+}
