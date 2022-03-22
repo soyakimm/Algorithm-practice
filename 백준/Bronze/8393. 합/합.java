8f3f5a879b93694997f6c96ebce46ca21c3e8e3a
soyakimm <100359222+soyakimm@users.noreply.github.com>
2022-03-23 00:38:05 +0900

    [Bronze III] Title: 합, Time: 132 ms, Memory: 14136 KB -BaekjoonHub

--- 백준/Bronze/8393. 합/합.java
+++ 백준/Bronze/8393. 합/합.java
@@ -1,17 +1,19 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
  
 public class Main {
- 
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
- 
-		int N = in.nextInt();
-		in.close();
-		int sum = 0;
+	public static void main(String[] args) throws IOException {
         
-		for (int i = 1; i <= N; i++) {
-			sum += i;
-		}
-		System.out.println(sum);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ 
+		int N = Integer.parseInt(br.readLine()); //파싱처리
+		br.close(); //버퍼닫기
+		int A = 0; 
+ 
+		for( int i = 1 ; i <= N ; i++ ) A+=i;
+ 
+		System.out.println(A);
 	}
 }
+ 
