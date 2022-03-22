aa269eeee8d53ee9d752384b8a6d6b0e126c472e
soyakimm <100359222+soyakimm@users.noreply.github.com>
2022-03-23 00:36:05 +0900

    [Bronze III] Title: 합, Time: 208 ms, Memory: 17740 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+ 
+public class Main {
+ 
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+ 
+		int N = in.nextInt();
+		in.close();
+		int sum = 0;
+        
+		for (int i = 1; i <= N; i++) {
+			sum += i;
+		}
+		System.out.println(sum);
+	}
+}
